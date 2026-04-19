5e4647f61d1e0e1608fda065003fae693a57152c
moshe1ch-kidi <moshe1.ch@gmail.com>
2026-04-19 18:29:37 +0300

    Add files via upload

--- src/blockly/blocks.ts
+++ src/blockly/blocks.ts
@@ -2,25 +2,11 @@ import * as Blockly from 'blockly';
 import 'blockly/blocks';
 import { FieldColour } from '@blockly/field-colour';
 import { FieldSlider } from '@blockly/field-slider';
+import { FieldBitmap } from '@blockly/field-bitmap';
 
-// רישום השדות התקינים
 Blockly.fieldRegistry.register('field_colour', FieldColour);
 Blockly.fieldRegistry.register('field_slider', FieldSlider);
-
-/**
- * תיקון עבור FieldBitmap:
- * יצירת מחלקה מקומית כדי למנוע תלות בחבילה חיצונית שבורה
- */
-class SimpleFieldBitmap extends Blockly.Field {
-  static fromJson(options: any) {
-    return new SimpleFieldBitmap(options['value']);
-  }
-  constructor(value: any) {
-    super(value);
-  }
-  override getText() { return "LEDs"; }
-}
-Blockly.fieldRegistry.register('field_bitmap', SimpleFieldBitmap);
+Blockly.fieldRegistry.register('field_bitmap', FieldBitmap);
 
 const ICONS: { [key: string]: number[] } = {
   'HEART': [1, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 22],
@@ -102,6 +88,8 @@ export const microbitBlocks = [
           [0, 0, 0, 0, 0],
           [0, 0, 0, 0, 0],
         ],
+        width: 5,
+        height: 5,
       },
     ],
     previousStatement: null,
@@ -138,3 +126,470 @@ export const microbitBlocks = [
   },
   {
     type: 'microbit_button_pressed',
+    message0: 'on button %1 pressed',
+    args0: [
+      {
+        type: 'field_dropdown',
+        name: 'BUTTON',
+        options: [
+          ['A', 'A'],
+          ['B', 'B'],
+          ['A+B', 'A+B'],
+        ],
+      },
+    ],
+    nextStatement: null,
+    colour: '#FFBF00',
+    hat: 'cap',
+  },
+  {
+    type: 'microbit_set_pin',
+    message0: 'set pin %1 to %2',
+    args0: [
+      { 
+        type: 'field_dropdown', 
+        name: 'PIN', 
+        options: [
+          ['J1', 'J1'], ['J2', 'J2'], ['J3', 'J3'], ['J4', 'J4']
+        ] 
+      },
+      { type: 'input_value', name: 'VALUE', check: 'Number' },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+    colour: '#4C97FF',
+  },
+  {
+    type: 'microbit_led_pin',
+    message0: 'LED %1 %2',
+    args0: [
+      { 
+        type: 'field_dropdown', 
+        name: 'PIN', 
+        options: [['J1', 'J1'], ['J2', 'J2'], ['J3', 'J3'], ['J4', 'J4']] 
+      },
+      { 
+        type: 'field_dropdown', 
+        name: 'STATE', 
+        options: [['ON', '1'], ['OFF', '0']] 
+      },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+    colour: '#4C97FF',
+  },
+  {
+    type: 'microbit_tm1637_show_number',
+    message0: '4-digit display %1 show number %2',
+    args0: [
+      { type: 'field_dropdown', name: 'PORT', options: [['J1', 'J1'], ['J2', 'J2'], ['J3', 'J3'], ['J4', 'J4']] },
+      { type: 'input_value', name: 'VALUE', check: 'Number' },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+    colour: '#4C97FF',
+  },
+  {
+    type: 'microbit_set_motor',
+    message0: 'set motor %1 %2 speed %3',
+    args0: [
+      { type: 'field_dropdown', name: 'PORT', options: [['M1', 'M1'], ['M2', 'M2'], ['M3', 'M3'], ['M4', 'M4']] },
+      { type: 'field_dropdown', name: 'DIRECTION', options: [['FD', 'FORWARD'], ['BK', 'BACKWARD']] },
+      { type: 'input_value', name: 'SPEED', check: 'Number' },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+    colour: '#4C97FF',
+  },
+  {
+    type: 'microbit_stop_motor',
+    message0: 'stop motor %1',
+    args0: [
+      { type: 'field_dropdown', name: 'PORT', options: [['M1', 'M1'], ['M2', 'M2'], ['M3', 'M3'], ['M4', 'M4']] },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+    colour: '#4C97FF',
+  },
+  {
+    type: 'microbit_set_servo',
+    message0: 'Servo %1 angle %2',
+    args0: [
+      { type: 'field_dropdown', name: 'PORT', options: [['S1', 'S1'], ['S2', 'S2'], ['S3', 'S3'], ['S4', 'S4']] },
+      { type: 'input_value', name: 'ANGLE', check: 'Number' },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+    colour: '#4C97FF',
+  },
+  {
+    type: 'microbit_ultrasonic_distance',
+    message0: 'Ultrasonic sensor %1 distance',
+    args0: [
+      { type: 'field_dropdown', name: 'PORT', options: [['J1', 'J1'], ['J2', 'J2'], ['J3', 'J3'], ['J4', 'J4']] },
+    ],
+    output: 'Number',
+    colour: '#9966FF',
+  },
+  {
+    type: 'microbit_color_sensor',
+    message0: 'Color sensor %1 color',
+    args0: [
+      { type: 'field_dropdown', name: 'PORT', options: [['I1', 'I1'], ['I2', 'I2'], ['I3', 'I3'], ['I4', 'I4']] },
+    ],
+    output: 'String',
+    colour: '#9966FF',
+  },
+  {
+    type: 'microbit_light_sensor',
+    message0: 'Light sensor %1 value',
+    args0: [
+      { type: 'field_dropdown', name: 'PORT', options: [['J1', 'J1'], ['J2', 'J2']] },
+    ],
+    output: 'Number',
+    colour: '#9966FF',
+  },
+  {
+    type: 'microbit_temperature_sensor',
+    message0: 'Temperature sensor %1 value',
+    args0: [
+      { type: 'field_dropdown', name: 'PORT', options: [['J1', 'J1'], ['J2', 'J2'], ['J3', 'J3'], ['J4', 'J4'], ['I1', 'I1'], ['I2', 'I2'], ['I3', 'I3'], ['I4', 'I4']] },
+    ],
+    output: 'Number',
+    colour: '#9966FF',
+  },
+  {
+    type: 'microbit_dht11',
+    message0: 'DHT11 %1 %2 value',
+    args0: [
+      { 
+        type: 'field_dropdown', 
+        name: 'MODE', 
+        options: [
+          ['Temperature', 'TEMP'],
+          ['Humidity', 'HUM']
+        ] 
+      },
+      { 
+        type: 'field_dropdown', 
+        name: 'PORT', 
+        options: [['J1', 'J1'], ['J2', 'J2']] 
+      },
+    ],
+    output: 'Number',
+    colour: '#9966FF',
+  },
+  {
+    type: 'microbit_soil_moisture',
+    message0: 'Soil Moisture sensor %1 value',
+    args0: [
+      { type: 'field_dropdown', name: 'PORT', options: [['J1', 'J1'], ['J2', 'J2'], ['J3', 'J3'], ['J4', 'J4']] },
+    ],
+    output: 'Number',
+    colour: '#9966FF',
+  },
+  {
+    type: 'microbit_potentiometer',
+    message0: 'Potentiometer %1 value',
+    args0: [
+      { type: 'field_dropdown', name: 'PORT', options: [['J1', 'J1'], ['J2', 'J2'], ['J3', 'J3'], ['J4', 'J4']] },
+    ],
+    output: 'Number',
+    colour: '#9966FF',
+  },
+  {
+    type: 'microbit_ledgraph',
+    message0: 'show graph %1 max %2',
+    args0: [
+      { type: 'input_value', name: 'VALUE', check: 'Number' },
+      { type: 'input_value', name: 'MAX', check: 'Number' },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+    colour: '#4C97FF',
+  },
+  {
+    type: 'microbit_play_tone',
+    message0: 'play tone %1',
+    args0: [
+      { type: 'input_value', name: 'TONE', check: 'Number' },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+    colour: '#4C97FF',
+  },
+  {
+    type: 'event_when_green_flag_clicked',
+    message0: 'when %1 clicked',
+    args0: [
+      {
+        type: 'field_image',
+        src: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGhlaWdodD0iNDBweCIgdmlld0JveD0iMCAtOTYwIDk2MCA5NjAiIHdpZHRoPSI0MHB4IiBmaWxsPSIjMkU3RDMyIj48cGF0aCBkPSJNMjAwLTEyMHYtNjgwaDM2MGwxNiA4MGgyMjR2NDAwSDUyMGwtMTYtODBIMjgwdjI4MGgtODBaIi8+PC9zdmc+',
+        width: 29,
+        height: 29,
+        alt: 'Green Flag'
+      }
+    ],
+    nextStatement: null,
+    colour: '#FFBF00',
+    hat: 'cap',
+  },
+  {
+    type: 'event_broadcast',
+    message0: 'broadcast message %1 %2',
+    args0: [
+      { type: 'field_dropdown', name: 'SHAPE', options: SHAPE_OPTIONS },
+      { type: 'field_dropdown', name: 'COLOR', options: COLOR_OPTIONS },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+    colour: '#FFBF00',
+  },
+  {
+    type: 'event_when_received',
+    message0: 'when message %1 %2 received',
+    args0: [
+      { type: 'field_dropdown', name: 'SHAPE', options: SHAPE_OPTIONS },
+      { type: 'field_dropdown', name: 'COLOR', options: COLOR_OPTIONS },
+    ],
+    nextStatement: null,
+    colour: '#FFBF00',
+    hat: 'cap',
+  },
+  {
+    type: 'control_repeat',
+    message0: 'repeat %1',
+    args0: [{ type: 'field_number', name: 'TIMES', value: 10 }],
+    message1: '%1',
+    args1: [{ type: 'input_statement', name: 'DO' }],
+    previousStatement: null,
+    nextStatement: null,
+    colour: '#FFAB19',
+  },
+  {
+    type: 'control_forever',
+    message0: 'forever',
+    message1: '%1',
+    args1: [{ type: 'input_statement', name: 'DO' }],
+    previousStatement: null,
+    colour: '#FFAB19',
+  },
+  {
+    type: 'control_wait_until',
+    message0: 'wait until %1',
+    args0: [{ type: 'input_value', name: 'CONDITION', check: 'Boolean' }],
+    previousStatement: null,
+    nextStatement: null,
+    colour: '#FFAB19',
+  },
+  {
+    type: 'control_wait',
+    message0: 'wait %1 seconds',
+    args0: [{ type: 'field_number', name: 'DURATION', value: 1 }],
+    previousStatement: null,
+    nextStatement: null,
+    colour: '#FFAB19',
+  },
+  {
+    type: 'control_if',
+    message0: 'if %1 then',
+    args0: [
+      {
+        type: 'input_value',
+        name: 'IF0',
+        check: 'Boolean',
+      },
+    ],
+    message1: '%1',
+    args1: [
+      {
+        type: 'input_statement',
+        name: 'DO0',
+      },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+    colour: '#FFAB19',
+  },
+  {
+    type: 'control_if_else',
+    message0: 'if %1 then',
+    args0: [
+      {
+        type: 'input_value',
+        name: 'IF0',
+        check: 'Boolean',
+      },
+    ],
+    message1: '%1',
+    args1: [
+      {
+        type: 'input_statement',
+        name: 'DO0',
+      },
+    ],
+    message2: 'else',
+    message3: '%1',
+    args3: [
+      {
+        type: 'input_statement',
+        name: 'ELSE',
+      },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+    colour: '#FFAB19',
+  },
+  {
+    type: 'logic_boolean',
+    message0: '%1',
+    args0: [
+      {
+        type: 'field_dropdown',
+        name: 'BOOL',
+        options: [
+          ['true', 'TRUE'],
+          ['false', 'FALSE']
+        ]
+      }
+    ],
+    output: 'Boolean',
+    colour: '#59C059'
+  },
+  {
+    type: 'operator_add',
+    message0: '%1 + %2',
+    args0: [
+      { type: 'input_value', name: 'NUM1' },
+      { type: 'input_value', name: 'NUM2' }
+    ],
+    inputsInline: true,
+    output: 'Number',
+    colour: '#59C059'
+  },
+  {
+    type: 'operator_subtract',
+    message0: '%1 - %2',
+    args0: [
+      { type: 'input_value', name: 'NUM1' },
+      { type: 'input_value', name: 'NUM2' }
+    ],
+    inputsInline: true,
+    output: 'Number',
+    colour: '#59C059'
+  },
+  {
+    type: 'operator_multiply',
+    message0: '%1 * %2',
+    args0: [
+      { type: 'input_value', name: 'NUM1' },
+      { type: 'input_value', name: 'NUM2' }
+    ],
+    inputsInline: true,
+    output: 'Number',
+    colour: '#59C059'
+  },
+  {
+    type: 'operator_divide',
+    message0: '%1 / %2',
+    args0: [
+      { type: 'input_value', name: 'NUM1' },
+      { type: 'input_value', name: 'NUM2' }
+    ],
+    inputsInline: true,
+    output: 'Number',
+    colour: '#59C059'
+  },
+  {
+    type: 'operator_random',
+    message0: 'pick random %1 to %2',
+    args0: [
+      { type: 'input_value', name: 'FROM' },
+      { type: 'input_value', name: 'TO' }
+    ],
+    inputsInline: true,
+    output: 'Number',
+    colour: '#59C059'
+  },
+  {
+    type: 'operator_lt',
+    message0: '%1 < %2',
+    args0: [
+      { type: 'input_value', name: 'OP1' },
+      { type: 'input_value', name: 'OP2' }
+    ],
+    inputsInline: true,
+    output: 'Boolean',
+    colour: '#59C059'
+  },
+  {
+    type: 'operator_equals',
+    message0: '%1 = %2',
+    args0: [
+      { type: 'input_value', name: 'OP1' },
+      { type: 'input_value', name: 'OP2' }
+    ],
+    inputsInline: true,
+    output: 'Boolean',
+    colour: '#59C059'
+  },
+  {
+    type: 'operator_gt',
+    message0: '%1 > %2',
+    args0: [
+      { type: 'input_value', name: 'OP1' },
+      { type: 'input_value', name: 'OP2' }
+    ],
+    inputsInline: true,
+    output: 'Boolean',
+    colour: '#59C059'
+  },
+  {
+    type: 'operator_and',
+    message0: '%1 and %2',
+    args0: [
+      { type: 'input_value', name: 'BOOL1', check: 'Boolean' },
+      { type: 'input_value', name: 'BOOL2', check: 'Boolean' }
+    ],
+    inputsInline: true,
+    output: 'Boolean',
+    colour: '#59C059'
+  },
+  {
+    type: 'operator_or',
+    message0: '%1 or %2',
+    args0: [
+      { type: 'input_value', name: 'BOOL1', check: 'Boolean' },
+      { type: 'input_value', name: 'BOOL2', check: 'Boolean' }
+    ],
+    inputsInline: true,
+    output: 'Boolean',
+    colour: '#59C059'
+  },
+  {
+    type: 'operator_not',
+    message0: 'not %1',
+    args0: [
+      { type: 'input_value', name: 'BOOL', check: 'Boolean' }
+    ],
+    inputsInline: true,
+    output: 'Boolean',
+    colour: '#59C059'
+  },
+  {
+    type: 'text',
+    message0: '" %1 "',
+    args0: [
+      {
+        type: 'field_input',
+        name: 'TEXT',
+        text: '',
+      },
+    ],
+    output: 'String',
+    colour: '#59C059',
+  },
+];
+
+// Register blocks
+Blockly.defineBlocksWithJsonArray(microbitBlocks);
